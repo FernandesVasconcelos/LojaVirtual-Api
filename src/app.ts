ba554ce77308d903d1b3678a39452da27930556c
JOAO VICTOR FERNANDES VASCONCELOS <a95716548@rn.senac.local>
2026-04-29 12:13:14 -0300

    Minha API nova de Loja Virtual

---
+++ src/app.ts
@@ -0,0 +1,79 @@
+const express = require("express"); 
+import type { Response, Request, NextFunction } from "express";
+import { promises as fs } from "fs";
+const app = express();
+const port = 3000;
+
+interface Produto {
+    id: number;
+    nome: string;
+    preco: number;
+    categoria: string;
+    estoque: number;
+    disponível: boolean;
+}
+
+interface CriarProdutoBody {
+    nome: string;
+    preco: number;
+    categoria: string;
+    estoque: number;
+}
+
+interface AtualizarProdutoBody {
+    nome?: string;
+    preco?: number;
+    categoria?: string;
+    estoque?: number;
+    disponível?: boolean;
+}
+
+interface ProdutoParams {
+    id: string;
+}
+
+interface FiltroQuery {
+    categoria?: string;
+    disponivel?: string;
+}
+
+interface ApiResponse<T> {
+    sucesso: boolean;
+    dados?: T;
+    erro?: string[];
+}
+
+let produto: Produto[] = [];
+let proximoId = 1
+
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+app.use(express.static("public"));
+app.use(express.json());
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const agora = new Date().toLocaleTimeString();
+    console.log(`[${agora}] ${req.method} ${req.url}`);
+    next();
+  });
+
+
+
+async function lerArquivo (): Promise<Produto[]>  {
+    try {
+        const texto = await fs.readFile("dados/produtos.json", "utf-8");
+        return JSON.parse(texto);
+    }
+    catch (erro) {
+        return [];
+    }
+}
+
+async function salvarArquivo (produtos: Produto): Promise<void> {
+    try {
+        const texto = JSON.stringify(produto, null, 2)
+        await fs.writeFile("dados/produtos.json", texto, "utf-8");
+    }
+    catch (erro) {
+        console.error("Erro ao salvar o arquivo:", erro);
+    }
+}
